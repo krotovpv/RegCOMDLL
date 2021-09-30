780620dedca6506cf2154ba2542c159ea8ea51d2
krotovpv <krotovpv@gmail.com>
2021-09-30 15:37:56 +0300

    first commit

---
+++ RegOPCDAAuto/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace RegOPCDAAuto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sysfolder = Environment.GetFolderPath(Environment.SpecialFolder.SystemX86);
+            string dllname = "OPCDAAuto.dll";
+
+            if (!File.Exists(sysfolder + "\\" + dllname))
+                Process.Start(sysfolder + "\\cmd.exe", "/C copy " + dllname + " " + sysfolder + " && regsvr32 " + dllname);
+            else
+                Process.Start(sysfolder + "\\cmd.exe", "/C regsvr32 " + dllname);
+        }
+    }
+}
---
+++ UnRegOPCDAAuto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace UnRegOPCDAAuto
+{
+    internal class Program
+    {
+        static string sysfolder = Environment.GetFolderPath(Environment.SpecialFolder.SystemX86);
+        static string dllname = "OPCDAAuto.dll";
+
+        static void Main(string[] args)
+        {
+            Process proc = Process.Start(sysfolder + "\\cmd.exe", "/C regsvr32 /U " + dllname + " && del " + sysfolder + "\\" + dllname);
+        }
+    }
+}
